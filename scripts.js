6366ce4994cda39b0c29a4d0e5146c1b2a57783e
akash4142 <yadavakki440@gmail.com>
2023-07-18 16:42:08 -0400

    initial commit

---
+++ scripts.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const fs = require('fs');
+
+
+module.exports.readDataFromFile = function(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      try {
+        const jsonData = JSON.parse(data);
+        resolve(jsonData);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  });
+}
