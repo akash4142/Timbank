85848c3c34b4d4a944b6cd4c2b613d9593261ace
akash4142 <yadavakki440@gmail.com>
2023-07-25 23:29:49 -0400

    initial commit

--- public/client_script.js
+++ public/client_script.js
@@ -1,66 +1,60 @@
-// Client-side JavaScript code
-
 // Cart to store selected items
 const cart = [];
 
 // Event listener for "Add to Cart" buttons
 const addToCartButtons = document.querySelectorAll('.add-to-cart-button');
 addToCartButtons.forEach((button) => {
-  console.log('hello from updatecart') 
   button.addEventListener('click', () => {
     const name = button.dataset.name;
     const price = button.dataset.price;
     cart.push({ name, price });
     updateCart();
-    
   });
 });
 
 // Function to update the cart items on the client-side
 function updateCart() {
-  const cartContainer = document.getElementById('cart-container');
-  const totalContainer = document.getElementById('total-container');
-  cartContainer.innerHTML = '';
-  console.log('hello from updatecart')
-  let totalPrice = 0;
+  const cartItemsContainer = document.querySelector('.cart-items-container');
+
+  // Clear the cart items container before updating
+  cartItemsContainer.innerHTML = '';
 
   cart.forEach((item) => {
     const cartItemDiv = document.createElement('div');
     cartItemDiv.classList.add('cart-item');
     cartItemDiv.textContent = `${item.name} - Price: ${item.price}`;
-    cartContainer.appendChild(cartItemDiv);
-
-    // Calculate the total price
-    totalPrice += parseFloat(item.price);
+    cartItemsContainer.appendChild(cartItemDiv);
   });
 
+  // Show the cart container on the right side of the screen
+  const cartContainer = document.querySelector('#cart-container');
+  cartContainer.style.display = 'block';
+
+  // Calculate the total price
+  const totalPrice = cart.reduce((total, item) => total + parseFloat(item.price), 0);
+
   // Display the total price
+  const totalContainer = document.querySelector('#total-container');
   totalContainer.textContent = `Total Price: $${totalPrice.toFixed(2)}`;
 }
 
-
-// client_Script.js
-
-// Function to calculate the total price from the cart items
-function calculateTotalPrice() {
-  let totalPrice = 0;
-console.log(totalPrice);
-  cart.forEach((item) => {
-    // Calculate the total price by adding the price of each item
-    totalPrice += parseFloat(item.price);
-   
-  });
-
-  return totalPrice;
-}
-
-
-
-// Event listener for "Get Started" button click
-const getStartedButton = document.querySelector('.main__btn');
-getStartedButton.addEventListener('click', () => {
-  // Redirect to the "/dashboard" path
-  window.location.href = '/dashboard';
+// Event listener for "Pay Now" button click
+const payNowButton = document.querySelector('#pay-now-button');
+payNowButton.addEventListener('click', () => {
+  // Implement the logic for payment processing here (e.g., redirect to a payment gateway).
+  // For this example, we will just reset the cart and hide the cart container.
+  cart.length = 0;
+  updateCart();
 });
 
+// Function to hide the cart container
+function hideCart() {
+  const cartContainer = document.querySelector('#cart-container');
+  cartContainer.style.display = 'none';
+}
 
+// Event listener for the close button in the cart container
+const closeButton = document.querySelector('.close-button');
+closeButton.addEventListener('click', () => {
+  hideCart();
+});
