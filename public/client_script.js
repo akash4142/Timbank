a430982191ea210bbd7b9483cb5f30896dc6f3b4
akash4142 <yadavakki440@gmail.com>
2023-07-26 10:23:49 -0400

    initial commit

--- public/client_script.js
+++ public/client_script.js
@@ -38,14 +38,14 @@ function updateCart() {
   totalContainer.textContent = `Total Price: $${totalPrice.toFixed(2)}`;
 }
 
-// Event listener for "Pay Now" button click
-const payNowButton = document.querySelector('#pay-now-button');
-payNowButton.addEventListener('click', () => {
-  // Implement the logic for payment processing here (e.g., redirect to a payment gateway).
-  // For this example, we will just reset the cart and hide the cart container.
-  cart.length = 0;
-  updateCart();
-});
+// // Event listener for "Pay Now" button click
+// const payNowButton = document.querySelector('#pay-now-button');
+// payNowButton.addEventListener('click', () => {
+//   // Implement the logic for payment processing here (e.g., redirect to a payment gateway).
+//   // For this example, we will just reset the cart and hide the cart container.
+//   cart.length = 0;
+//   updateCart();
+// });
 
 // Function to hide the cart container
 function hideCart() {
@@ -53,8 +53,41 @@ function hideCart() {
   cartContainer.style.display = 'none';
 }
 
+
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+
+menu.addEventListener('click', function () {
+  console.log('menu buttton clicked')
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
+
+
+function hideCart() {
+  const cartContainer = document.getElementById('cart-container');
+  cartContainer.style.display = 'none';
+}
+
 // Event listener for the close button in the cart container
 const closeButton = document.querySelector('.close-button');
-closeButton.addEventListener('click', () => {
-  hideCart();
-});
+if (closeButton) { // Check if closeButton exists before adding the event listener
+  closeButton.addEventListener('click', () => {
+    hideCart();
+  });
+}
+
+// Event listener for the "Get Started" button
+const getStartedButton = document.querySelector('.main__btn');
+if (getStartedButton) { // Check if getStartedButton exists before adding the event listener
+  getStartedButton.addEventListener('click', function () {
+    window.location.href = 'dashboard';
+  });
+}
+
+
+
+
+
+
+
